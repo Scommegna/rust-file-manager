552b4f0deacb7323d258e66aaabe39a920a27984
Lucas Scommegna <scommegnal@hotmail.com>
2026-05-11 20:13:06 -0300

    feat: changed way that cli function works to be a loop

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,82 @@
-mod cli;
 mod filesystem;
-mod search;
-mod tree;
-mod benchmark;
-mod utils;
 
-use clap::Parser;
-use cli::{Cli, Commands};
+use std::io::{self, Write};
+use std::path::Path;
 
-fn main() -> std::io::Result<()> {
-    let cli = Cli::parse();
-    
-    match cli.command { 
-        Commands::List { dir } => {
-            filesystem::list_dir(&dir)?;
+fn main() {
+    loop {
+        print!("fm> ");
+
+        io::stdout()
+            .flush()
+            .expect("Error to clean output buffer");
+
+        let mut input = String::new();
+
+        if io::stdin().read_line(&mut input).is_err() {
+            eprintln!("Error to read command.");
+            continue;
         }
-        Commands::Info { path } => {
-            filesystem::show_info(&path)?;
+
+        let input = input.trim();
+
+        if input.is_empty() {
+            continue;
+        }
+
+        if input == "exit" {
+            break;
+        }
+
+        handle_command(input);
+    }
+}
+
+fn handle_command(input: &str) {
+    let parts: Vec<&str> = input.split_whitespace().collect();
+
+    let command = parts[0];
+
+    match command {
+        "help" => show_help(),
+
+        "list" => {
+            if parts.len() != 2 {
+                eprintln!("Usage: list <dir>");
+                return;
+            }
+
+            let path = Path::new(parts[1]);
+
+            if let Err(error) = filesystem::list_dir(path) {
+                eprintln!("Error to list directory: {}", error);
+            }
+        },
+
+        "info" => {
+            if parts.len() != 2 {
+                eprintln!("Usage: info <file or dir>");
+                return;
+            }
+
+            let path = Path::new(parts[1]);
+
+            if let Err(error) = filesystem::show_info(path) {
+                eprintln!("Error to obtain information: {}", error);
+            }
+        },
+
+        _ => {
+            eprintln!("Unknown command: {}", command);
+            eprintln!("Type 'help' to show available commands");
         }
     }
-    
-    Ok(())
+}
+
+fn show_help() {
+    println!("Available Commands:");
+    println!("  list <dir>                    List files of directory.");
+    println!("  info <file or dir>            Show information of file or directory.");
+    println!("  help                          Show help message.");
+    println!("  exit                          Quits file manager.");
 }
